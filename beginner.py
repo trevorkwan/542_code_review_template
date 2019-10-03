b38ab3ee16ce2b1390e625d4198235a69bcda03a
trevorkwan <54831831+trevorkwan@users.noreply.github.com>
2019-10-03 10:49:44 -0700

    fake edit

--- beginner.py
+++ beginner.py
@@ -1,5 +1,6 @@
 # Beginner function(s)
 ## Code Attribution: Code snippets taken and adapted from https://towardsdatascience.com/how-to-be-pythonic-and-why-you-should-care-188d63a5037e
+#new small change
 
 def sum_of_a_range(a=10,b=1000):
     """ 
